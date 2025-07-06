593bed1e950483e8d3624d09233adc73e40d6b6d
Hans Christian Schmitz <git@hcsch.eu>
2025-07-06 18:58:09 +0200

    Plot with/without balloon in separate rows

--- plot-measurements.py
+++ plot-measurements.py
@@ -3,32 +3,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 CONFIGS_KIND = ["hermit", "linux"]
 CONFIGS_NUM_PARALLEL = [1, 2, 4]
 
 fig, ax_grid = plt.subplots(
     figsize=(12, 8),
-    nrows=len(CONFIGS_KIND),
+    nrows=len(CONFIGS_KIND) * 2,
     ncols=len(CONFIGS_NUM_PARALLEL),
     sharey=True,
     sharex=True,
 )
 
-for kind, ax_row in zip(CONFIGS_KIND, ax_grid):
+for (kind, balloon_name), ax_row in zip(
+    itertools.product(
+        CONFIGS_KIND,
+        ["without-balloon", "with-balloon"],
+    ),
+    ax_grid,
+):
     for n, ax in zip(CONFIGS_NUM_PARALLEL, ax_row):
-        ax.set_title(f"{kind} ×{n}", loc="left", fontstyle="oblique", fontsize="medium")
+        ax.set_title(
+            f"{kind} ×{n} {balloon_name}",
+            loc="left",
+            fontstyle="oblique",
+            fontsize="medium",
+        )
 
-        for with_balloon, balloon_name in [
-            (False, "without-balloon"),
-            (True, "with-balloon"),
-        ]:
-            measurements = pd.read_csv(f"measurements/{kind}-{balloon_name}-{n}.csv")
-            # Convert RSS from kibibytes to Gibibytes
-            measurements.rss = measurements.rss.astype("float64") / (1024 * 1024)
+        measurements = pd.read_csv(f"measurements/{kind}-{balloon_name}-{n}.csv")
+        # Convert RSS from kibibytes to Gibibytes
+        measurements.rss = measurements.rss.astype("float64") / (1024 * 1024)
 
-            measurements.groupby("pid").plot(
-                "elapsed_s", "rss", ax=ax, xlabel="t [s]", ylabel="RSS [GiB]"
-            )
+        measurements.groupby("pid").plot(
+            "elapsed_s", "rss", ax=ax, xlabel="t [s]", ylabel="RSS [GiB]"
+        )
 
 plt.savefig(f"measurements/plot.svg")
