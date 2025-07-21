c37e38605941e0d2ff44bfb65e14128a4f7eeb15
Hans Christian Schmitz <git@hcsch.eu>
2025-07-21 21:42:52 +0200

    plots: redo timing plots

--- plot-measurements.py
+++ plot-measurements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from typing import Optional
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -60,15 +61,10 @@ fig.tight_layout()
 fig.savefig(f"measurements/plot.svg")
 
 
-fig, ax_grid = plt.subplots(
-    figsize=(12, 12),
-    nrows=len(CONFIGS_KIND) * len(CONFIGS_BALLOON) * len(CONFIGS_AMP),
-    ncols=len(CONFIGS_NUM_PARALLEL),
-    sharex=True,
-)
+timings: Optional[pd.DataFrame] = None
 
-for (kind, balloon_name, amp_name), ax_row in zip(
-    itertools.product(CONFIGS_KIND, CONFIGS_BALLOON, CONFIGS_AMP),
+for (amp_name, kind, balloon_name), ax_row in zip(
+    itertools.product(CONFIGS_AMP, CONFIGS_KIND, CONFIGS_BALLOON),
     ax_grid,
 ):
     for n, ax in zip(CONFIGS_NUM_PARALLEL, ax_row):
@@ -79,19 +75,61 @@ for (kind, balloon_name, amp_name), ax_row in zip(
             fontsize="medium",
         )
 
-        timings = pd.read_csv(
+        single_timings = pd.read_csv(
             f"measurements/{kind}-{balloon_name}-{amp_name}-{n}-timings.csv"
         )
-        timings.pid = timings.pid.astype("str")
-        timings = timings.set_index("pid")
 
-        ax.barh(
-            y=timings.index,
-            width=timings["workload_runtime_s"],
+        single_timings["kind"] = kind
+        single_timings["with_balloon"] = balloon_name
+        single_timings["with_amp"] = amp_name
+        single_timings["num_parallel"] = n
+
+        single_timings = single_timings.set_index(
+            ["num_parallel", "with_amp", "kind", "with_balloon", "pid"]
         )
 
-        ax.set_xlabel("t [s]")
+        if timings is None:
+            timings = single_timings
+        else:
+            timings = pd.concat([timings, single_timings])
 
+print(timings)
+
+mean_timings = timings.groupby(
+    by=["num_parallel", "with_amp", "kind", "with_balloon"]
+).mean()
+
+mean_timings["rel_overhead"] = (
+    mean_timings["workload_runtime_s"]
+    / mean_timings.loc[1, "without-amp", "hermit", "without-balloon"][
+        "workload_runtime_s"
+    ]
+    - 1.0
+) * 100.0
+
+fig, ax_grid = plt.subplots(
+    figsize=(6, 8),
+    nrows=len(CONFIGS_AMP) * len(CONFIGS_BALLOON),
+    ncols=len(CONFIGS_KIND),
+    sharey=True,
+    sharex=True,
+)
+
+for (amp_name, balloon_name), ax_row in zip(
+    itertools.product(CONFIGS_AMP, CONFIGS_BALLOON),
+    ax_grid,
+):
+    for kind, ax in zip(CONFIGS_KIND, ax_row):
+        ax.set_title(
+            f"{kind} {balloon_name} {amp_name}",
+            loc="left",
+            fontstyle="oblique",
+            fontsize="medium",
+        )
+
+        plot = mean_timings["rel_overhead"][:, amp_name, kind, balloon_name].plot(
+            kind="bar", rot=0, grid=True, ylabel="exec time overhead [%]", ax=ax
+        )
 
 fig.tight_layout()
 fig.savefig(f"measurements/timings.svg")
